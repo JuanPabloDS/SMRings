be60dd680167c9fbb6ef9c59a26d89ae5d5dae88
JuanPabloDS <jpsevero30@hotmail.com>
2022-07-07 13:19:38 -0300

    Atualizando login e cadastro

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views # para autenticação e logout
+from .views import login_page, register_page
+
 
 urlpatterns = [
-    # path('', view, name=""),
-    path('login/', auth_views.LoginView.as_view(
+    path('login/', login_page, name='login'),
+    path('cadastro/', register_page, name='cadastro'),
+]
+
+"""
+path('login/', auth_views.LoginView.as_view(
         template_name='usuarios/form.html'
     ), name='login'),
-    path('cadastro/', auth_views.LoginView.as_view(
+
+path('cadastro/', auth_views.LoginView.as_view(
         template_name='usuarios/cadastro.html'
-    ), name='cadastro'),
-]
+    ), name='cadastro'),"""
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, get_user_model
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages  # Importar o messages
 
-# Create your views here.
+
+from .forms import LoginForm, RegisterForm
+
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+                    "form": form
+              }
+    print("User logged in")
+    #print(request.user.is_authenticated)
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password) 
+        print(user)
+        #print(request.user.is_authenticated)
+        if user is not None:
+            #print(request.user.is_authenticated)
+            login(request, user)
+            print("Login válido")
+            # Redireciona para uma página de sucesso.
+            return redirect("/")
+        else:
+            #Retorna uma mensagem de erro de 'invalid login'.
+            print("Login inválido")
+    return render(request, "usuarios/login.html", context)
+
+User = get_user_model()
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+                    "form": form
+              }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
+        # messages.success(request, 'Cadastro feito com sucesso!' )
+        return redirect("/login/")
+    return render(request, "usuarios/cadastro.html", context)
