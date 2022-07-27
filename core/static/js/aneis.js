456126b95bdbade4dc5c152140c2ec3efb5da1e8
JuanPabloDS <jpsevero30@hotmail.com>
2022-07-27 09:23:13 -0300

    Corrigindo formato dos preços

--- core/static/js/aneis.js
+++ core/static/js/aneis.js
@@ -9,3 +9,10 @@ function tamanho_aneis() {
 function remove_item() {
      sessionStorage.removeItem('carrinho', '1')
 }
+
+
+function real_br_money(din) {
+     var money = parseInt(din)
+     var formate = money.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+     document.write(formate)
+}
