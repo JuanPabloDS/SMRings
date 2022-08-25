58346688eabe1f12e8dee0f232b5a0c3021b5785
JuanPabloDS <jpsevero30@hotmail.com>
2022-08-25 15:02:56 -0300

    Mudando nome do projeto

--- smrings/wsgi.py
+++ smrings/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for amrings project.
+WSGI config for smrings project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amrings.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smrings.settings')
 
 application = get_wsgi_application()
